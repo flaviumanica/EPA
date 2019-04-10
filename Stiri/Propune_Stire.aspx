3e848df5f4949ac94aa5f3f1dc34330caed92e41
antoniobarbalau <antoniobarbalau@gmail.com>
2019-04-10 12:37:25 +0300

    8) Ca utilizator, vreau sa pot propune stiri, care vor necesita aprobare

---
+++ Stiri/Propune_Stire.aspx
@@ -0,0 +1,60 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="Propune_Stire.aspx.cs" Inherits="Propune_Stire" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+    <link rel="Stylesheet" href="/Posteaza_Stire.css" type="text/css" />
+    <div class="tot">
+         <h1> Propune stire </h1>
+        <asp:Literal ID="Succes" runat="server"> </asp:Literal>
+        
+        <br />
+        <asp:Label class="txt" ID="LTitlu" runat="server" Text="Titlu" AssociatedControlID="Titlu"></asp:Label>
+        <br />
+        <asp:TextBox ID="Titlu" class="textbox" runat="server"></asp:TextBox>
+        <asp:RequiredFieldValidator
+                    ID="TitluRequired"
+                    runat="server"
+                    ControlToValidate="Titlu"
+                    ErrorMessage="Titlu obligatoriu!"
+                    ForeColor="Red">*</asp:RequiredFieldValidator>
+        <br />
+        <asp:Label class="txt" ID="Label2" runat="server" Text="Continut" AssociatedControlID="Continut"></asp:Label>
+        <br />
+        <asp:TextBox ID="Continut" style="width:400px; height:250px;" class="textbox" runat="server" TextMode="multiline" Columns="50" Rows="5"></asp:TextBox>
+        <asp:RequiredFieldValidator
+                    ID="RequiredFieldValidator2"
+                    runat="server"
+                    ControlToValidate="Continut"
+                    ErrorMessage="Continut obligatoriu!"
+                    ForeColor="Red">*</asp:RequiredFieldValidator>
+        <br />
+        <asp:Label class="txt" ID="Label5" runat="server" Text="Categorie"></asp:Label>
+        <br />
+        <asp:DropDownList ID="DDL" class="ddl"
+            runat="server" 
+            DataSourceID="SqlDataSource1" 
+            DataTextField="Nume" 
+            DataValueField="Id">
+        </asp:DropDownList>
+        <asp:SqlDataSource ID="SqlDataSource1" 
+            runat="server"
+            ConnectionString="Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename='D:\Master\Sem. 2\ElemProgrAvansata\Proiect\Prezentare\Stiri\App_Data\Database.mdf';Integrated Security=True" 
+            SelectCommand="SELECT [Id], [Nume] FROM [Categorii]"></asp:SqlDataSource><br /><br />
+        <asp:Label class="txt" ID="Label6" runat="server" Text="Adauga Categorie Noua"></asp:Label>
+        <br />
+        <asp:TextBox ID="AdaugaCategorie" class="textbox" runat="server"></asp:TextBox>
+        <br /> <br />
+        <asp:Literal ID="Mesaj" runat="server"></asp:Literal>
+        <label class="file-upload">
+            <span><strong>Adauga imagine</strong></span>
+        <asp:FileUpload ID="Image" runat="server" > </asp:FileUpload>
+            </label>
+        <br />
+        
+        <asp:Button class="but" ID="Button1" runat="server" Text="Propune stire!" OnClick="Posteaza"/>
+
+    </div>
+    <asp:Literal ID="Literal1" runat="server"> </asp:Literal>
+</asp:Content>
+
---
+++ Stiri/Propune_Stire.aspx.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+
+public partial class Propune_Stire : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Session["user"] == null)
+        {
+            Response.Redirect("~/Acces.aspx");
+        }
+        
+    }
+    protected void Posteaza(object sender, EventArgs e)
+    {
+        //Mesaj.Text = DropDownList1.SelectedValue;
+        SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename='D:\Master\Sem. 2\ElemProgrAvansata\Proiect\Prezentare\Stiri\App_Data\Database.mdf';Integrated Security=True");
+        con.Open();
+        try
+        {
+            string titlu = Titlu.Text;
+            string continut = Continut.Text;
+            string categorie = "";
+            int ID_Categorie = 0;
+            string sursa_img = null;
+
+            if (AdaugaCategorie.Text.Length > 0)//daca introduce categorie noua;
+            {
+                categorie = AdaugaCategorie.Text;
+                string queryCategorie = "INSERT INTO [Categorii] (Nume) VALUES (@categorie); SELECT CAST(scope_identity() AS int)";
+                SqlCommand com1 = new SqlCommand(queryCategorie, con);
+                com1.Parameters.AddWithValue("categorie", categorie);
+                ID_Categorie = (Int32)com1.ExecuteScalar();
+            }
+            else
+            {
+                ID_Categorie = Int32.Parse(DDL.SelectedValue);
+                
+            }
+
+ 
+            
+            string date = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+            int user = Int32.Parse(Session["id"].ToString());
+            string query = "INSERT INTO [Stiri_propuse] (Titlu_propunere, Continut_propunere, User_propunere, Data_propunere, Categorie_propunere, Imagine_propunere) "+
+                           "VALUES (@titlu, @continut, @user, @date, @categorie, @img); SELECT CAST(scope_identity() AS int) ";
+            SqlCommand com = new SqlCommand(query, con);
+            com.Parameters.AddWithValue("titlu", titlu);
+            com.Parameters.AddWithValue("continut", continut);
+            com.Parameters.AddWithValue("date", date);
+            com.Parameters.AddWithValue("user", user);
+            com.Parameters.AddWithValue("categorie", ID_Categorie);
+            if (Image.HasFile)
+            {
+                if (Image.PostedFile.ContentType.ToLower().EndsWith("jpeg") || Image.PostedFile.ContentType.ToLower().EndsWith("png"))
+                {
+                    //introduc imaginea in baza de date
+                    Random random = new Random();
+                    int rand = random.Next(0, 1000000000);
+                    int rand1 = random.Next(0, 1000000000);
+                    sursa_img = "~/Imagini_Stiri_Propuse/" + rand + rand1 + ".jpg";
+                    
+
+                    //salvez dupa id.
+                    Image.SaveAs(Server.MapPath("~") + "/Imagini_Stiri_Propuse/" + rand + rand1 + ".jpg");
+                }
+                else
+                    Mesaj.Text = "Image file is not in JPEG format! Format is: " + Image.PostedFile.ContentType.ToUpper();
+            }
+            else
+            {
+                Mesaj.Text = "No image file uploaded!";
+            }
+            com.Parameters.AddWithValue("img", sursa_img);
+
+            //int IDUL = (Int32)com.ExecuteScalar();
+            com.ExecuteNonQuery();
+            //Mesaj.Text = "Id-ul este" + IDUL.ToString();
+
+            
+            
+            con.Close();
+        }
+        catch (Exception ex)
+        {
+            Mesaj.Text = "Database insert error : " + ex.Message;
+        }
+        finally
+        {
+            con.Close();
+            //Succes.Text = "Stire adaugata cu succes!!!";
+            //Titlu.Text = " ";
+            //Continut.Text = " ";
+            //AdaugaCategorie.Text = " ";
+            Response.Redirect("~/Home.aspx");
+
+
+        }
+    }
+}
