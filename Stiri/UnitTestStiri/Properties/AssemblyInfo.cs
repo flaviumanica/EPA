a69885bcf59d44ac3706a18801881ddf59fe835b
Iuliana Georgescu <georgescu_lily@yahoo.com>
2019-04-10 21:18:39 +0300

    Test-1 testing add article

---
+++ Stiri/UnitTestStiri/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestStiri")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestStiri")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c6bf6ae7-1f56-4c07-b282-9517cd0b856d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Stiri/UnitTestStiri/UnitTest1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Stiri;
+using Stiri.Old_App_Code;
+using System;
+
+namespace UnitTestStiri
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        IFake TestContext = new Stiri.StiriFakeDb();
+        string key = "2345";
+        int keyAddedArticle = -1; 
+
+      
+        // This test checks if an artical is inserted correctly in the db. 
+        [TestMethod]  
+        public void TestAddArticol()
+        { 
+            // create our Repository
+            Repository repository = new Repository(); 
+            string titlu = "Titlul articolului" + key;
+            string continut = "Continutul articolului" + key;
+            string descriere = "Descriere" + key;
+            int categorie = 2;
+            string link = "link" + key;
+            User user = null;
+            DateTime dataPublicare = new DateTime();
+            // we've inserted our first article, with a null user, it must return -1
+            int result = repository.AdaugareArticol(TestContext, categorie, titlu, continut, descriere, link, dataPublicare, user);
+            Assert.IsTrue(result == -1);
+            user = new User();
+            keyAddedArticle = repository.AdaugareArticol(TestContext, categorie, titlu, continut, descriere, link, dataPublicare, user);
+            Assert.IsTrue(keyAddedArticle != -1);
+            // now, we want to check every field we filled  
+            Assert.IsTrue(TestContext.Articol.Where(a => a.Titlu.Equals(titlu)).ToList().Count() > 0);
+            Assert.IsTrue(TestContext.Articol.Where(a => a.Continut.Equals(continut)).ToList().Count() > 0);
+            Assert.IsTrue(TestContext.Articol.Where(a => a.Descriere.Equals(descriere)).ToList().Count() > 0);
+            Assert.IsTrue(TestContext.Articol.Where(a => a.Categorie.Equals(categorie)).ToList().Count() > 0);  
+        }
+
+         
+
+    }
+}
